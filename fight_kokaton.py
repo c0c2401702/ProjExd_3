99ae9fc6550c82425e125f5e8734a7093c339e10
c0c2401702 <c0c2401702@edu.teu.ac.jp>
2025-10-07 15:05:06 +0900

    練習３

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -171,6 +171,7 @@ def main():
                 if beam.rct.colliderect(bomb.rct):
                     #ビームと爆弾の衝突判定
                     beam, bomb =None,None
+                    bird.change_img(6, screen)
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam is not None:
